a68fe0092c28986d4a5bc092062f91e222b4bfae
Miho Yamamoto <miyamam@users.noreply.github.com>
2022-08-12 14:38:13 +0900

    Update train.py

--- src/model/train.py
+++ src/model/train.py
@@ -39,9 +39,13 @@ def get_csvs_df(path):
 
 # add function to split data
 def split_data(df):
-    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', 'TricepsThickness', \
-        'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, df['Diabetic'].values
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=0)
+    X, y = df[['Pregnancies', 'PlasmaGlucose', 'DiastolicBloodPressure', \
+    'TricepsThickness', 'SerumInsulin', 'BMI', 'DiabetesPedigree', 'Age']].values, \
+    df['Diabetic'].values
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, \
+    test_size=0.30, random_state=0)
+    
     return X_train, X_test, y_train, y_test
 
 
@@ -66,8 +70,9 @@ def parse_args():
     # return args
     return args
 
+
 # run script
-if __name__ == "__main__":
+if __name__ == "__main__": 
     # add space in logs
     print("\n\n")
     print("*" * 60)
